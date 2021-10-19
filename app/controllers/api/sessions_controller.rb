9fe84f61fe1268d2c6c5e17b315915d2ae363746
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-10-19 15:16:07 -0700

    add to develop notes in sessions controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,9 +1,13 @@
 class Api::SessionsController < ApplicationController
   def create
-    
+    #find user by credentials
+    #if user is nil, render :new view
+    #else, login redirect to user_url
   end
 
   def destroy
-    
+    #logout
+    #render empty {} upon successful logout
+    #render a 404 message if no current_user to logout
   end
 end
