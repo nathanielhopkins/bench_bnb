ea1f871172ce9919f87f143066e249ad7ed80b22
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-10-19 16:19:57 -0700

    write create and destory methods in sessions_controller.rb using new User and application controller methods

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -2,12 +2,29 @@ class Api::SessionsController < ApplicationController
   def create
     #find user by credentials
     #if user is nil, render :new view
-    #else, login redirect to user_url
+    #else, login render api/users/show
+    @user = User.find_by_credentials(
+      username: params[:user][:username], 
+      password: params[:user][:password]
+    )
+    if @user.nil
+      render json: ["Invalid username/password combination"], status: 401
+    else
+      login(@user)
+      render "api/users/show"
+    end
   end
 
   def destroy
     #logout
     #render empty {} upon successful logout
     #render a 404 message if no current_user to logout
+    @user = current_user
+    if @user
+      logout
+      render "api/users/show"
+    else
+      render json: ["Nobody signed in"], status: 404
+    end
   end
 end
