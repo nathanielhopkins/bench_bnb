0f8cb904d552e29f14356a7ab739f9d5a7f9a1e5
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-10-20 16:25:30 -0700

    add App.jsx and root.jsx to our frontend/components folder ; in App, write a functional component with with a h1 as a placeholder; in Root, write a functional component that renders a Provider wrapping a HashRouter wrapping App

---
+++ frontend/components/root.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+
+const Root = ({ store }) => (
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>
+);
