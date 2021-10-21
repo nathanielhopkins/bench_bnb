7d42bc4acd218b9ad7b828c89363ff464c2cfe6d
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-10-21 15:57:56 -0700

    import AuthRoute in app.jsx; replace Routes in App with AuthRoutes

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { Route } from "react-router";
 import GreetingContainer from './greeting/greeting_container';
 import LoginFormContainer from './session_form/login_form_container';
 import SignupFormContainer from './session_form/signup_form_container';
+import { AuthRoute } from "../util/route_util";
 
 const App = () => (
   <div>
@@ -11,8 +11,8 @@ const App = () => (
       <GreetingContainer />
     </header>
 
-    <Route path="/login" component={LoginFormContainer} />
-    <Route path="/signup" component={SignupFormContainer} />
+    <AuthRoute path="/login" component={LoginFormContainer} />
+    <AuthRoute path="/signup" component={SignupFormContainer} />
   </div>
 );
 
