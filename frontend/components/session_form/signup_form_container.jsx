50cde63b75e102a75b06c63de686686ad63a99ca
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-10-20 17:40:02 -0700

    add currentUser to mapStateToProps in both LoginFormContainer and SignupFormContainer that references state.session;

--- frontend/components/session_form/signup_form_container.jsx
+++ frontend/components/session_form/signup_form_container.jsx
@@ -1,10 +1,11 @@
 import { connect } from 'react-redux';
 import { signup } from '../../actions/session_actions';
+import SessionForm from './session_form';
 
 const mapStateToProps = (state, ownProps) => ({
   errors: state.errors,
   formType: 'signup',
-  ownProps
+  currentUser: state.session
 });
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
