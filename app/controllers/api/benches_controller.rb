b054daa8846ae32f04e8fd8e2e22a0632a2acb11
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-10-27 15:13:19 -0700

    fix error in Api::BenchesController#create: change bench to @bench in conditional save statement

--- app/controllers/api/benches_controller.rb
+++ app/controllers/api/benches_controller.rb
@@ -8,7 +8,7 @@ class Api::BenchesController < ApplicationController
   def create
     @bench = Bench.new(bench_params)
 
-    if bench.save
+    if @bench.save
       render :show
     else
       render json: @bench.errors.full_messages, status: 401
