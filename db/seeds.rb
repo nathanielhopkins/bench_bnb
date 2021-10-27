6125b5fa0f0a44a5b6ab9b26060646806f840e1d
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-10-27 14:37:16 -0700

    fix typo in seeds that had attr long instead of lng

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,6 @@
 
 #Bench seeds
 
-b1 = Bench.create!(lat: 37.772699, long: -122.464355, description: 'Beautiful green stretch bench in Golden Gate Park')
-b2 = Bench.create!(lat: 37.759114, long: -122.422802, description: 'Modest bench among a row of benches; courtside view of tennis at Mission Playground')
-b3 = Bench.create!(lat: 37.784809691653955, long: -122.40222970016015, description: 'Backless two-seater circling Yerba Buena Gardens') 
+b1 = Bench.create!(lat: 37.772699, lng: -122.464355, description: 'Beautiful green stretch bench in Golden Gate Park')
+b2 = Bench.create!(lat: 37.759114, lng: -122.422802, description: 'Modest bench among a row of benches; courtside view of tennis at Mission Playground')
+b3 = Bench.create!(lat: 37.784809691653955, lng: -122.40222970016015, description: 'Backless two-seater circling Yerba Buena Gardens') 
