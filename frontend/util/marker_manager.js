7f752a78fa6102dd1d1b428232e605fef4d4de4a
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-10-29 12:20:18 -0700

    write helper createMarkerFromBench(bench) in MarkerManger that takes in a bench, creates a new marker for that bench on the map, and then adds it to this.markers object; edit updateMarkers to enumerate through passed benches and any benches that are not in this.markers, call createMarkerFromBench on it

--- frontend/util/marker_manager.js
+++ frontend/util/marker_manager.js
@@ -2,9 +2,26 @@ export default class MarkerManager {
   constructor(map) {
     this.map = map;
     this.markers = {};
+
+    this.updateMarkers = this.updateMarkers.bind(this);
   }
 
   updateMarkers(benches) {
-    console.log('time to update');
+    benches.forEach(bench => {
+      if (!this.markers.hasOwnProperty(bench.id)) {
+        this.createMarkerFromBench(bench);
+      };
+    });
+    window.markers = this.markers
+  }
+
+  createMarkerFromBench(bench) {
+    let map = this.map
+    let newMarker = new google.maps.Marker({
+      position: { lat: bench.lat, lng: bench.lng },
+      map,
+    });
+    
+    this.markers[bench.id] = newMarker
   }
 }
