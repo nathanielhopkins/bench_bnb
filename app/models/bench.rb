5cd0d5d18cb2a9c2dbb4959c31d08d42ed04ffb0
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-10-29 15:21:15 -0700

    write method Bench.in_bounds in Bench model that takes in a set of google map bounds and returns all Benches within those bounds

--- app/models/bench.rb
+++ app/models/bench.rb
@@ -1,3 +1,12 @@
 class Bench < ApplicationRecord
   validates :description, :lat, :lng, presence: true
+
+  def self.in_bounds(bounds)
+    # Filters all benches and returns those within passed bounds
+    filtered = Bench.all.select do |bench| 
+      (bench[:lat].between?(bounds['southWest']['lat'].to_f, bounds['northEast']['lat'].to_f)) and (bench[:lng].between?(bounds['southWest']['lng'].to_f, bounds['northEast']['lng'].to_f))
+    end
+    
+    return filtered
+  end
 end
