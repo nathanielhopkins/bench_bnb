9c78af77f46dfb01a881b2991e4daee0835cbbe6
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-11-01 13:49:42 -0700

    update updateMarkers in MarkerManager to create a benches object from this.benches array; iterate through this.markers and remove any from this.markers and from map that dont correspond to a bench in benchesObj

--- frontend/util/marker_manager.js
+++ frontend/util/marker_manager.js
@@ -7,9 +7,24 @@ export default class MarkerManager {
   }
 
   updateMarkers(benches) {
+    // initialize bench object as empty, will fill when iterating through benches
+    const benchesObj = {};
+
+    // iterate through benches, create a marker for each bench that doesn't already have one
     benches.forEach(bench => {
+      benchesObj[bench.id] = bench;
       if (!this.markers.hasOwnProperty(bench.id)) {
         this.createMarkerFromBench(bench);
+      };
+    });
+    
+    // iterate through markers and remove any that don't correspond to benchesObj
+    const markersArray = Object.entries(this.markers);
+    markersArray.forEach(marker => {
+      let markerObj = marker[0];
+      if(!benchesObj.hasOwnProperty(markerObj)) {
+        this.markers[markerObj].setMap(null);
+        delete this.markers[markerObj];
       };
     });
   }
