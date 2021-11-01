13fe6bd5c71f023793e0ea905958ad26ff69fe44
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-11-01 13:57:06 -0700

    remove passing fetchBenches to mapDispatchToProps in SearchContainer

--- frontend/components/bench/search_container.js
+++ frontend/components/bench/search_container.js
@@ -8,7 +8,6 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  fetchBenches: () => dispatch(fetchBenches()),
   updateBounds: bounds => dispatch(updateBounds(bounds))
 });
 
