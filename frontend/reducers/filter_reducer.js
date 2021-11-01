b65961a8a58fc7a862b9e4e3def532efc8db4f60
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-11-01 14:33:39 -0700

    modify filterReducer to return {bounds: action.bounds} in switch case UPDATE_BOUNDS

--- frontend/reducers/filter_reducer.js
+++ frontend/reducers/filter_reducer.js
@@ -4,7 +4,7 @@ const filterReducer = (state = {}, action) => {
   Object.freeze(state);
   switch(action.type) {
     case UPDATE_BOUNDS:
-      return action.bounds;
+      return { bounds: action.bounds };
     default:
       return state;
   };
