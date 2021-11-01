c6385f0795af0c5c625604c0d34ca26882e20d1c
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-11-01 11:59:40 -0700

    update Search component to pass updateBounds to BenchMap

--- frontend/components/bench/search.jsx
+++ frontend/components/bench/search.jsx
@@ -4,12 +4,12 @@ import BenchMap from './bench_map';
 
 class Search extends React.Component {
   render() {
-    const { benches, fetchBenches } = this.props;
+    const { benches, fetchBenches, updateBounds } = this.props;
     
     return(
       <div className='user-pane'>
         <div className='left-pane'>
-          <BenchMap benches={benches} />
+          <BenchMap benches={benches} updateBounds={updateBounds} />
         </div>
         <div className='right-pane'>
           <BenchIndex benches={benches}  fetchBenches={fetchBenches} />
