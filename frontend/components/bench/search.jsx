70b20145dd9e8a056f74b0fd7e641c1442e34dc9
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-11-02 15:38:22 -0700

    update Search component to import FilterForm and render it in "right-pane" above BenchIndex, passing minSeating, maxSeating, and updateFilter as props

--- frontend/components/bench/search.jsx
+++ frontend/components/bench/search.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import BenchIndex from './bench_index';
 import BenchMap from './bench_map';
+import FilterForm from './filter_form'
 
 class Search extends React.Component {
   render() {
-    const { benches, updateFilter } = this.props;
+    const { benches, updateFilter, minSeating, maxSeating } = this.props;
     
     return(
       <div className='user-pane'>
@@ -12,6 +13,11 @@ class Search extends React.Component {
           <BenchMap benches={benches} updateFilter={updateFilter} />
         </div>
         <div className='right-pane'>
+          <FilterForm 
+            minSeating={minSeating} 
+            maxSeating={maxSeating} 
+            updateFilter={updateFilter}
+          />
           <BenchIndex benches={benches} />
         </div>
       </div>
