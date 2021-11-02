bbe5ba35dbce705b97b77af00e506970da49991b
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-11-01 17:23:48 -0700

    write and export action constant RECEIVE_BENCH, action creator receiveBench, and thunk action creator createBench in bench_actions.js

--- frontend/actions/bench_actions.js
+++ frontend/actions/bench_actions.js
@@ -1,4 +1,5 @@
 export const RECEIVE_BENCHES = 'RECEIVE_BENCHES';
+export const RECEIVE_BENCH = 'RECEIVE_BENCH';
 
 import * as APIUtil from "../util/bench_api_util";
 
@@ -7,7 +8,17 @@ export const receiveBenches = benches => ({
   benches
 });
 
+export const receiveBench = bench => ({
+  type: RECEIVE_BENCH,
+  bench
+});
+
 export const fetchBenches = (filters) => dispatch => {
   APIUtil.fetchBenches(filters)
     .then(benches => dispatch(receiveBenches(benches)));
+};
+
+export const createBench = bench => dispatch => {
+  APIUtil.createBench(bench)
+    .then(bench => dispatch(receiveBench(bench)));
 };
