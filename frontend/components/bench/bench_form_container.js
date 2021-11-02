4e5090953b066521d466bba3c679813686d5e576
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-11-01 17:10:03 -0700

    add lat and lng to mapStateToProps, using URLSearchParams(location.search) to get values from url params

--- frontend/components/bench/bench_form_container.js
+++ frontend/components/bench/bench_form_container.js
@@ -1,12 +1,9 @@
 import { connect } from 'react-redux';
 import BenchForm from './bench_form';
 
-const mapStateToProps = state => ({
-
+const mapStateToProps = (state, { location }) => ({
+  lat: new URLSearchParams(location.search).get("lat"),
+  lng: new URLSearchParams(location.search).get("lng")
 });
 
-const mapDispatchToProps = dispatch => ({
-
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(BenchForm);
+export default connect(mapStateToProps, null)(BenchForm);
