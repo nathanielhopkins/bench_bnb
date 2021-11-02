6851aa3ca8af34685d567d05479f7d0e3f7a8bc9
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-11-02 13:35:38 -0700

    remove deprecated UPDATE_BOUNDS action constant and action creators

--- frontend/actions/filter_actions.js
+++ frontend/actions/filter_actions.js
@@ -1,25 +1,12 @@
-export const UPDATE_BOUNDS = 'UPDATE_BOUNDS';
 export const UPDATE_FILTER = 'UPDATE_FILTER';
 
 import { fetchBenches } from "./bench_actions";
-
-export const actionUpdateBounds = bounds => ({
-  type: UPDATE_BOUNDS,
-  bounds
-});
 
 export const changeFilter = (filter, value) => ({
   type: UPDATE_FILTER,
   filter,
   value
 });
-
-// export const updateBounds = bounds => {
-//   return (dispatch, getState) => {
-//     dispatch(actionUpdateBounds(bounds));
-//     return fetchBenches(getState().ui.filters)(dispatch)
-//   };
-// }
 
 export function updateFilter(filter, value) {
   return (dispatch, getState) => {
