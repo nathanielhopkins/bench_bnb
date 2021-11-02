36da0518caf26162f2b4ec313f9f2979f9e3af1b
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-11-02 11:58:31 -0700

    write new function Protected that creates a Route that only renders its component if loggedIn is true, else redirects to /login; export const ProtectedRoute that connects Protected withRouter to mapStateToProps (which passes loggedIn from state)

--- frontend/util/route_util.jsx
+++ frontend/util/route_util.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Redirect } from "react-router-dom";
 import { withRouter } from "react-router";
 import { connect } from 'react-redux';
+import { Component } from 'react';
 
 const Auth = ({ component: Component, path, loggedIn, exact }) => (
   <Route
@@ -13,10 +14,24 @@ const Auth = ({ component: Component, path, loggedIn, exact }) => (
     />
 );
 
+const Protected = ({ component: Component, path, loggedIn, exact }) => (
+  <Route
+    path={path}
+    exact={exact}
+    render={props =>
+      loggedIn ? <Component {...props} /> : <Redirect to="/login" />
+    }
+  />
+);
+
 const mapStateToProps = state => {
   return { loggedIn: Boolean(state.session.id )};
 };
 
 export const AuthRoute = withRouter(
   connect(mapStateToProps, null)(Auth)
+);
+
+export const ProtectedRoute = withRouter(
+  connect(mapStateToProps)(Protected)
 );
