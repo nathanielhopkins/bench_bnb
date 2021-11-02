456fc11b787ce8ffe626ee9c45238a320e265431
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-11-02 12:42:58 -0700

    write and export new action constant UPDATE_FILTER in bench_actions.js; write and export new action creator updateFilter that takes in a filter and a value and returns an action object with type: UPDATE_FILTER and filter and value returned

--- frontend/actions/bench_actions.js
+++ frontend/actions/bench_actions.js
@@ -1,5 +1,6 @@
 export const RECEIVE_BENCHES = 'RECEIVE_BENCHES';
 export const RECEIVE_BENCH = 'RECEIVE_BENCH';
+export const UPDATE_FILTER = 'UPDATE_FILTER';
 
 import * as APIUtil from "../util/bench_api_util";
 
@@ -12,6 +13,12 @@ export const receiveBench = bench => ({
   type: RECEIVE_BENCH,
   bench
 });
+
+export const updateFilter = (filter, value) => ({
+  type: UPDATE_FILTER,
+  filter,
+  value
+})
 
 export const fetchBenches = (filters) => dispatch => {
   APIUtil.fetchBenches(filters)
