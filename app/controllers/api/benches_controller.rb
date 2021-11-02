ddde5037ad7764a694dc4f6f3948b2d1ad9d7f40
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-11-02 12:37:55 -0700

    modify bench_params in api/benches controller to accept :max_seating and :min_seating as params; modify index to filter @benches by params[:max_seating] and params[:min_seating] if present

--- app/controllers/api/benches_controller.rb
+++ app/controllers/api/benches_controller.rb
@@ -1,7 +1,13 @@
 class Api::BenchesController < ApplicationController
   def index
     @benches = Bench.in_bounds(params[:bounds])
-  
+    if(params[:max_seating])
+      @benches = @benches.select { |bench| bench.seating < params[:max_seating] }
+    end
+    if(params[:min_seating])
+      @benches = @benches.select { |bench| bench.seating > params[:min_seating] }
+    end
+
     render :index
   end
 
@@ -17,6 +23,6 @@ class Api::BenchesController < ApplicationController
 
   private
   def bench_params
-    params.require(:bench).permit(:description, :lat, :lng, :seating)
+    params.require(:bench).permit(:description, :lat, :lng, :seating, :max_seating, :min_seating)
   end
 end
