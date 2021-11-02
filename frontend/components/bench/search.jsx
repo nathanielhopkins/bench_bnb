60328c77fc761c000e1e5bfddf8c16da58b854b0
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-11-02 13:32:10 -0700

    pass updateFilter to BenchMap in Search component

--- frontend/components/bench/search.jsx
+++ frontend/components/bench/search.jsx
@@ -4,12 +4,12 @@ import BenchMap from './bench_map';
 
 class Search extends React.Component {
   render() {
-    const { benches, updateBounds } = this.props;
+    const { benches, updateFilter } = this.props;
     
     return(
       <div className='user-pane'>
         <div className='left-pane'>
-          <BenchMap benches={benches} updateBounds={updateBounds} />
+          <BenchMap benches={benches} updateFilter={updateFilter} />
         </div>
         <div className='right-pane'>
           <BenchIndex benches={benches} />
