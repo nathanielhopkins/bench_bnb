e38dde7eef6f56052d8340481b1cc47a03de5b63
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-11-03 11:10:08 -0700

    remove lat and lng from render in BenchIndexItem

--- frontend/components/bench/bench_index_item.jsx
+++ frontend/components/bench/bench_index_item.jsx
@@ -5,10 +5,6 @@ const BenchIndexItem = ({ bench }) => {
     <div className='index-item-info'>
       <span className='index-item-category'>Description:</span>
       <span className='index-item-copy'>{bench.description}</span>
-      <span className='index-item-category'>Latitude:</span>
-      <span className='index-item-copy'>{bench.lat}</span>
-      <span className='index-item-category'>Longitude:</span>
-      <span className='index-item-copy'>{bench.lng}</span>
     </div>
   );
 };
