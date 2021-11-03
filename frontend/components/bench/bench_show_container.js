76913a6399c93d958d5e4115685e357eee9c73ae
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-11-03 12:26:52 -0700

    import fetchBench from bench_actions.js and pass it to mapDispatchToProps in BenchShowContainer

--- frontend/components/bench/bench_show_container.js
+++ frontend/components/bench/bench_show_container.js
@@ -1,8 +1,13 @@
 import { connect } from 'react-redux';
 import BenchShow from './bench_show';
+import { fetchBench } from '../../actions/bench_actions';
 
 const mapStateToProps = ( state, ownProps ) => ({
   benchId: ownProps.match.params.benchId
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchBench: benchId => dispatch(fetchBench(benchId))
 })
 
-export default connect(mapStateToProps, null)(BenchShow)
+export default connect(mapStateToProps, mapDispatchToProps)(BenchShow)
