c159be8cc65f5e4dc09b2d06c100aa81eac3ab87
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-11-03 14:27:19 -0700

    update mapStateToProps in BenchShowContainer to retrieve and return bench and benchId as props

--- frontend/components/bench/bench_show_container.js
+++ frontend/components/bench/bench_show_container.js
@@ -2,9 +2,14 @@ import { connect } from 'react-redux';
 import BenchShow from './bench_show';
 import { fetchBench } from '../../actions/bench_actions';
 
-const mapStateToProps = ( state, ownProps ) => ({
-  benchId: ownProps.match.params.benchId
-});
+const mapStateToProps = ( state, ownProps ) => {
+  const benchId = parseInt(ownProps.match.params.benchId);
+  const bench = state.entities.benches[benchId];
+  return({
+    benchId,
+    bench
+  })
+};
 
 const mapDispatchToProps = dispatch => ({
   fetchBench: benchId => dispatch(fetchBench(benchId))
