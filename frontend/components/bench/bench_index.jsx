9521fcd95c3a730a4dc702823986e6ddada3d478
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-11-03 15:37:34 -0700

    update BenchIndex to add an onClick handler to each BenchIndexItem that redirects to that bench show page

--- frontend/components/bench/bench_index.jsx
+++ frontend/components/bench/bench_index.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { withRouter } from 'react-router';
 import BenchIndexItem from './bench_index_item';
 
 class BenchIndex extends React.Component {
+  handleClick(benchId) {
+    const url = `/benches/${benchId}`
+    this.props.history.push(url);
+  }
+
   render() {
     const benches = this.props.benches.map(bench => (
-      <BenchIndexItem key={bench.id} bench={bench} />
+      <div 
+        onClick={() => this.handleClick(bench.id)}
+        key={bench.id}
+        className='bench-index-item'
+        >
+      <BenchIndexItem  bench={bench} />
+      </div>
     ));
     return (
       <div>
@@ -15,4 +27,4 @@ class BenchIndex extends React.Component {
   }
 }
 
-export default BenchIndex;
+export default withRouter(BenchIndex);
