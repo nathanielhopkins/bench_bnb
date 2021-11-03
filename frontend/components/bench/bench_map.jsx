bccb3889840004eac2a64efca437f7ea6a4d3f6e
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-11-03 14:23:50 -0700

    update BenchMap to move addListener logic to helper method; add logic for optional prop singleBench that creates marker for singleBench and centers and zooms on marker, without adding map event listeners

--- frontend/components/bench/bench_map.jsx
+++ frontend/components/bench/bench_map.jsx
@@ -6,17 +6,43 @@ class BenchMap extends React.Component {
 
   componentDidMount() {
     // set the map to show SF
-    const mapOptions = this.props.mapCenter || {
-      center: { lat: 37.7758, lng: -122.435 }, // this is SF
-      zoom: 12
+    const mapOptions = {
+        center: { lat: 37.7758, lng: -122.435 }, // this is SF
+        zoom: 12
     };
 
     // wrap this.mapNode in a Google Map
     this.map = new google.maps.Map(this.mapNode, mapOptions);
     // create a new MarkerManager
     this.MarkerManager = new MarkerManager(this.map);
-    this.MarkerManager.updateMarkers(this.props.benches);
+    if(this.props.singleBench) {
+      this.props.fetchBench(this.props.benchId);
+    } else {
+      this.addMarkerListeners();
+      this.MarkerManager.updateMarkers(this.props.benches);
+    }
 
+  };
+
+  componentDidUpdate() {
+    if(this.props.singleBench) {
+      // set marker for single bench
+      const benchKey = Object.keys(this.props.benches)[0];
+      const bench = this.props.benches[benchKey];
+      this.MarkerManager.updateMarkers([bench]);
+      // set new map options to center and zoom on single bench
+      const newMapOptions = {
+        center: { lat: bench.lat, lng: bench.lng},
+        zoom: 13
+      };
+      this.map.setOptions(newMapOptions);
+    } else {
+      // set markers for all benches in area
+      this.MarkerManager.updateMarkers(this.props.benches);
+    }
+  }
+
+  addMarkerListeners() {
     // create idle listener that calls updateFilter with new bounds
     this.map.addListener("idle", () => {
       // retrieve raw bounds data and package as bounds for API
@@ -24,8 +50,8 @@ class BenchMap extends React.Component {
       let nE = rawBounds.getNorthEast();
       let sW = rawBounds.getSouthWest();
       let bounds = {
-        "northEast": {"lat": nE.lat(), "lng": nE.lng()},
-        "southWest": {"lat": sW.lat(), "lng": sW.lng()}
+        "northEast": { "lat": nE.lat(), "lng": nE.lng() },
+        "southWest": { "lat": sW.lat(), "lng": sW.lng() }
       };
       // dispatch new bounds with updateFilter
       this.props.updateFilter('bounds', bounds);
@@ -34,11 +60,7 @@ class BenchMap extends React.Component {
     // add click handler to map
     this.map.addListener('click', (e) => {
       this.handleClick(e.latLng);
-    })
-  };
-
-  componentDidUpdate() {
-    this.MarkerManager.updateMarkers(this.props.benches);
+    });
   }
 
   handleClick(coords) {
