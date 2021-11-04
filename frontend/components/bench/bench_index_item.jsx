d992356792b315f713544362d218f960400b7800
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-11-04 16:10:14 -0700

    add rating to BenchIndexItem render

--- frontend/components/bench/bench_index_item.jsx
+++ frontend/components/bench/bench_index_item.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 const BenchIndexItem = ({ bench }) => {
   return(
     <div className='index-item-info'>
+      <span className='index-item-category'>Rating:</span>
+      <br/>
+      <span className='index-item-copy'>{bench.rating}</span>
+      <br/>
       <span className='index-item-category'>Description:</span>
+      <br/>
       <span className='index-item-copy'>{bench.description}</span>
     </div>
   );
