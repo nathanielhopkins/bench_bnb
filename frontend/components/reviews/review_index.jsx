1efda0d01527e5f7e6cbb1ff70e4e0ecac1dd1f3
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-11-04 15:12:13 -0700

    rewrite ReviewIndex as React Component; include componentDidMount that calls fetchBenchReviews from props, passing benchId; render waits for reviews to load then renders list of reviews with their rating and comment

--- frontend/components/reviews/review_index.jsx
+++ frontend/components/reviews/review_index.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 
-const ReviewIndex = ({reviews}) => {
-  const reviewsList = null;
-  
-  return(
-    <div className="reviews">
-      <h3>Reviews:</h3>
-      {reviewsList}
-    </div>
-  )
-};
+class ReviewIndex extends React.Component {
+  componentDidMount() {
+    this.props.fetchBenchReviews(this.props.benchId);
+  }
+
+  render() {
+    const { reviews } = this.props;
+    let reviewsList;
+    if (reviews) {
+      const reviewsArr = Object.values(reviews);
+      reviewsList = reviewsArr.map(review => {
+        return (
+          <ul key={review.id}>
+            <li>Rating: {review.rating}</li>
+            <li>Comment: {review.comment}</li>
+          </ul>
+        )
+      })
+    }
+    return(
+      <div className="reviews">
+        <h3>Reviews:</h3>
+        {reviewsList}
+      </div>
+    )
+  }
+}
 
 export default ReviewIndex;
