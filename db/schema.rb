73d377841d2856f41bc1bec704b4ee74d1ec477d
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-11-04 11:46:28 -0700

    generate model Review, write and run migration to include rating and commment field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_01_214242) do
+ActiveRecord::Schema.define(version: 2021_11_04_184319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 2021_11_01_214242) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "seating"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating", null: false
+    t.text "comment", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
