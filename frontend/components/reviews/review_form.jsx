ed57208216b30e9bf2e7ae1d5201ff44d5dea239
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-11-05 15:45:39 -0700

    update ReviewForm to be a React component; write render, including a form with inputs for rating and comment, which are stored in state and call a helper method to update; add a submit button that uses helper method handleSubmit; set rate to 5, comment to blank, author to this.props.author, and benchId to this.props.benchId in this.state in constructor

--- frontend/components/reviews/review_form.jsx
+++ frontend/components/reviews/review_form.jsx
@@ -1,13 +1,64 @@
 import React from 'react';
+import { render } from 'react-dom';
 import { Link } from 'react-router-dom';
 
-const ReviewForm = ({benchId, author, createBenchReview}) => {
+class ReviewForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state={
+      rating: 5,
+      comment: "",
+      author: this.props.author,
+      benchId: this.props.benchId
+    };
+    this.update = this.update.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-  return(
-    <div className='review-form'>
-      <Link to={`/benches/${benchId}`}><button>Cancel</button></Link>
-    </div>
-  )
+  update(property) {
+    return e => this.setState({[property]: e.target.value })
+  }
+
+  handleSubmit() {
+    return e => console.log(this.state);
+  }
+
+  render() {
+    const { benchId } = this.props;
+    return(
+      <div className='review-form'>
+        <h3>Leave a Review:</h3>
+        <label>Rating:
+          <br/>
+          <input 
+            type='number' 
+            min='1'
+            max='5'
+            value={this.state.rating}
+            onChange={this.update('rating')}
+            className="review-form-rating"
+          />
+        </label>
+        <label>Comment:
+          <br/>
+          <textarea 
+            cols='30'
+            rows='10'
+            onChange={this.update('comment')}
+            value={this.state.comment}
+            className='review-form-comment'
+            />
+        </label>
+        <button 
+          onClick={this.handleSubmit()}
+          className='review-form-submit'
+          >Submit Review</button>
+        <Link to={`/benches/${benchId}`}>
+          <button className='review-form-cancel'>Cancel</button>
+        </Link>
+      </div>
+    )
+  }
 };
 
 export default ReviewForm;
