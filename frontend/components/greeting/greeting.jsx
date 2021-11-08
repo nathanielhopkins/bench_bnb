82d2dea3ec832d6f87e1b2557a4f95b714f49211
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-11-08 14:47:00 -0800

    add hover effect for Login and SignUp links in main header

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 const Greeting = ({ currentUser, logout }) => {
   const sessionLinks = () => (
     <nav className="login-signup">
-      <Link to="/login">Login</Link>
+      <Link to="/login"><span>Login</span></Link>
       &nbsp;or&nbsp;
-      <Link to="/signup">Sign up!</Link>
+      <Link to="/signup"><span>Sign up!</span></Link>
     </nav>
   );
 
