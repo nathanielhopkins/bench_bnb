c686c0b7fbdc46053982ebf5b956ab2c03d830fb
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-11-08 14:43:53 -0800

    add styling for BenchIndexItem

--- frontend/components/bench/bench_index_item.jsx
+++ frontend/components/bench/bench_index_item.jsx
@@ -4,11 +4,8 @@ const BenchIndexItem = ({ bench }) => {
   return(
     <div className='index-item-info'>
       <span className='index-item-category'>Rating:</span>
-      <br/>
       <span className='index-item-copy'>{bench.rating}</span>
-      <br/>
       <span className='index-item-category'>Description:</span>
-      <br/>
       <span className='index-item-copy'>{bench.description}</span>
     </div>
   );
