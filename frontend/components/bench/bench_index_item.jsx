0bb821f7e488c7c758d6d7b526c8bddfddffeff0
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-11-10 16:15:23 -0800

    add images and styling to BenchIndexItems for root page

--- frontend/components/bench/bench_index_item.jsx
+++ frontend/components/bench/bench_index_item.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 
 const BenchIndexItem = ({ bench }) => {
   return(
-    <div className='index-item-info'>
-      <span className='index-item-category'>Rating:</span>
-      <span className='index-item-copy'>{bench.rating}</span>
-      <span className='index-item-category'>Description:</span>
-      <span className='index-item-copy'>{bench.description}</span>
+    <div className='bench-index-container'>
+      <div className='index-item-info'>
+        <span className='index-item-category'>Rating:</span>
+        <span className='index-item-copy'>{bench.rating}</span>
+        <span className='index-item-category'>Description:</span>
+        <span className='index-item-copy'>{bench.description}</span>
+      </div>
+      <div>
+        <img src={bench.photoUrl} className="bench-item-photo"/>
+      </div>
     </div>
   );
 };
