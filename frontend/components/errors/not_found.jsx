22446ba7a24af7f6376993bc00502ee7a028b4c7
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-11-12 13:06:56 -0800

    edit NotFound to render error message with an image and link back to root

--- frontend/components/errors/not_found.jsx
+++ frontend/components/errors/not_found.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return(
     <div className='not-found'>
-      <p>Something went wrong here.</p>
+      <h2>Sorry, something went wrong. Cannot find the page you're looking for.</h2>
+      <img 
+        src='https://c.tenor.com/8ApsefKQh2gAAAAC/starwars-droids.gif'
+        alt='star wars: these arent the droids youre looking for'
+      />
+      <Link to='/' >
+        <button className='error-nav-link'>Back to Page</button>
+      </Link>
     </div>
   )
 };
