2a438ae73c99231fc4798d8d4b586f80c86aeaea
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-11-15 14:18:57 -0800

    pass errors to mapStateToProps in ReviewFormContainer as state.errors.review; pass clearReviewErrors to mapDispatchToProps

--- frontend/components/reviews/review_form_container.js
+++ frontend/components/reviews/review_form_container.js
@@ -1,14 +1,16 @@
 import { connect } from 'react-redux';
 import ReviewForm from './review_form';
-import { createBenchReview } from '../../actions/bench_actions';
+import { createBenchReview, clearReviewErrors } from '../../actions/bench_actions';
 
 const mapStateToProps = (state, ownProps) => ({
   benchId: ownProps.benchId,
-  author: state.entities.users[state.session.id].username
+  author: state.entities.users[state.session.id].username,
+  errors: state.errors.review
 });
 
 const mapDispatchToProps = dispatch => ({
-  createBenchReview: review => dispatch(createBenchReview(review))
+  createBenchReview: review => dispatch(createBenchReview(review)),
+  clearReviewErrors: () => dispatch(clearReviewErrors())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ReviewForm)
